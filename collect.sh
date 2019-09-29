2f030a486bd1675fcf3395862a144b5489c6fb69
autostory <50351724+autostory@users.noreply.github.com>
2019-09-29 11:27:21 -0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -4,7 +4,7 @@ do
 echo "###################`date`#################################" >> /root/SFD_CPU_Mem_report.txt
 echo "`sfd 'system health --full' quit| sed -n '7p'`" >> /root/SFD_CPU_Mem_report.txt
 echo "`sfd 'system health --full' quit| sed -n '9p'`" >> /root/SFD_CPU_Mem_report.txt
-sleep 60
+sleep 120
 done
 
 
